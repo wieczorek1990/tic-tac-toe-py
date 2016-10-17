33c414704f5bf71aebd0f15bdf9187931bb9cb07
Łukasz Wieczorek <wieczorek1990@gmail.com>
2016-10-18 00:18:07 +0200

    Implemented checking winner with functional style

--- game.py
+++ game.py
@@ -2,7 +2,41 @@
 
 import json
 
-fp = open('board.json')
-board = json.load(fp)
-print board
+
+def load():
+  fp = open('board.json')
+  return json.load(fp)
+
+def winner(board):
+  n = len(board)
+  for col in board:
+    if n != len(col):
+      raise ValueError("Bad dimension size")
+
+  rows = board
+
+  cols = list(zip(*board))
+
+  ascending = range(n)
+  descending = reversed(ascending)
+  iterators = (zip(ascending, ascending), zip(descending, ascending))
+  diags = [[board[row][col] for row, col in iterator] for iterator in iterators]
+
+  paths = rows + cols + diags
+
+  for player in (1, 2):
+    for path in paths:
+      same = map(lambda p: player == p, path)
+      if all(same):
+        return player
+
+  return 0
+
+board = load()
+print(board)
+player = winner(board)
+if player != 0:
+  print("Player {} won".format(player))
+else:
+  print("No winner")
 
